1ea53ac9d692555cd30b5eafbb06102a4954537d
Dherrom0706 <dionihernandezromera@gmail.com>
2023-01-26 11:54:27 +0100

    mas o menos funciona

---
+++ app/src/main/java/www/iesmurgi/firebaseproyectodionisio/MainActivity.kt
@@ -0,0 +1,145 @@
+package www.iesmurgi.firebaseproyectodionisio
+
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import www.iesmurgi.firebaseproyectodionisio.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
+    companion object {
+        private const val RC_SIGN_IN = 423
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+
+
+        val btnIniciar = binding.btnSesion
+        btnIniciar.setOnClickListener {
+            var email = binding.tvUser.text
+            var pass = binding.tvPass.text
+            iniciarSesion(email.toString(), pass.toString())
+        }
+
+
+        val btnRegistro = binding.btnRegistro
+        //btnRegistro.setOnClickListener { crearUsuario(email.toString(), pass.toString()) }
+
+        /*val btnGoogle = binding.imagenIniciarConGoogle
+        btnGoogle.setOnClickListener { iniciarSesionGoogle() }
+
+         */
+
+    }
+
+    /*private fun iniciarSesionGoogle() {
+        val providerGoogle = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providerGoogle).build(),
+            Companion.RC_SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == Companion.RC_SIGN_IN){
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK){
+                val user = FirebaseAuth.getInstance().currentUser
+
+                startActivity(Intent(this,PerfilActivity::class.java))
+                finish()
+
+            }
+        }
+    }*/
+
+    override fun onStart() {
+        super.onStart()
+        if (auth.currentUser != null) {
+            startActivity(Intent(this, PerfilActivity::class.java))
+        }
+    }
+
+    fun crearUsuario(email: String, clave: String) {
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+            email,
+            clave
+        ).addOnCompleteListener {
+            if (it.isSuccessful) {
+                //writeNewUser(email)
+                abrirPerfil()
+            } else {
+                showErrorAlert()
+            }
+        }
+
+    }
+
+    /* private fun writeNewUser(email: String) {
+         val db = Firebase.firestore
+
+         val data = hashMapOf(
+             "email" to email,
+             "usuario" to "nouser",
+             "nacionalidad" to "nonacionalidad",
+             "edad" to 0
+         )
+
+         db.collection("user").document(email)
+             .set(data)
+             .addOnSuccessListener { Log.d(ContentValues.TAG,"DocumentSnapshot successfully written!") }
+             .addOnFailureListener { e -> Log.w(ContentValues.TAG, "Error writting document", e) }
+     }
+
+     */
+
+    fun iniciarSesion(email: String, clave: String) {
+
+
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                email,
+                clave
+            ).addOnCompleteListener {
+                if (it.isSuccessful) {
+
+                    abrirPerfil()
+                } else {
+                    Toast.makeText(this, "Intento de login fallido", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
+
+    }
+
+    private fun showErrorAlert() {
+        Toast.makeText(this, "Registro fallido", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun abrirPerfil() {
+        startActivity(Intent(this, PerfilActivity::class.java))
+        finish()
+    }
+}
---
+++ app/src/main/java/www/iesmurgi/firebaseproyectodionisio/PerfilActivity.kt
@@ -0,0 +1,73 @@
+package www.iesmurgi.firebaseproyectodionisio
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import www.iesmurgi.firebaseproyectodionisio.databinding.ActivityPerfilBinding
+
+class PerfilActivity : AppCompatActivity() {
+
+    val auth:FirebaseAuth = FirebaseAuth.getInstance()
+    val user:FirebaseUser? = auth.currentUser
+
+    private lateinit var binding: ActivityPerfilBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityPerfilBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        if (user != null){
+            setUp(user?.email.toString())
+        }
+
+        var btnRestablecerContra = binding.btnRecuperarContra
+        btnRestablecerContra.setOnClickListener {
+            restablecerClave()
+        }
+
+        var btnBorrarUsuario = binding.btnBorrarUser
+        btnBorrarUsuario.setOnClickListener {
+            borrarUsuario()
+        }
+
+    }
+
+    private fun borrarUsuario() {
+        user?.delete()?.addOnCompleteListener {
+
+            if (it.isSuccessful){
+                startActivity(Intent(this,MainActivity::class.java))
+                Toast.makeText(this,this.resources.getString(R.string.borrar_completado),Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this,it.exception.toString(),Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+    }
+
+    private fun restablecerClave() {
+        auth.sendPasswordResetEmail(user?.email.toString()).addOnCompleteListener {
+            if (it.isSuccessful){
+                Toast.makeText(this,this.resources.getString(R.string.msg_restablecer),Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this,it.exception.toString(),Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun setUp(email: String) {
+
+        var correo = binding.tvCorreo
+        correo.text = email
+
+        binding.btnCerrar.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+
+    }
+}
